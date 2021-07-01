0e0ba90cdcdf52ffc47378a74d2de2c011e06e60
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2021-07-01 21:09:27 +0200

    Removed old connection test and added comments

--- ModuleName/Invoke-Script.ps1
+++ ModuleName/Invoke-Script.ps1
@@ -9,37 +9,26 @@ function Invoke-Script{
         [ScriptBlock]$ScriptBlock
     )
     foreach($ComputerName in $ComputerNames){
-        # Test if system is reachable
-        if((Test-Connection -ComputerName $ComputerName  -Count 1 -ErrorAction SilentlyContinue)){
-            try {
-
-                Invoke-Command -computerName $ComputerName  -Credential $Cred -ScriptBlock $ScriptBlock
-            }
-            catch {
-                throw $_
-                Write-Output "Faild to Invoke Script!!"
-            }
+        try {
+            Invoke-Command -computerName $ComputerName  -Credential $Cred -ScriptBlock $ScriptBlock
+        }
+        catch {
+            throw $_
+            Write-Output "Faild to Invoke Script!!"
         }
     }
 }
-
-#$ComputerNames = Get-ADComputer -Filter * -SearchBase "OU=Win10,OU=WinDesktops,OU=Desktops,OU=Profidata_Computers,OU=Profidata,DC=profidata,DC=com" | Select-Object -ExpandProperty DNSHostName
-#$ComputerNames = Get-ADComputer -Filter * -SearchBase "OU=Urdorf,OU=Virtual Workstations,OU=Win10,OU=Windows,OU=Workstations,OU=Profidata_Computers,OU=Profidata,DC=profidata,DC=com" | Select-Object -ExpandProperty DNSHostName
-#$ComputerNames = Get-ADComputer -Filter * -SearchBase "OU=Romania,OU=Virtual Workstations,OU=Win10,OU=Windows,OU=Workstations,OU=Profidata_Computers,OU=Profidata,DC=profidata,DC=com" | Select-Object -ExpandProperty DNSHostName
-#$ComputerNames = Get-ADComputer -Filter * -SearchBase "OU=Frankfurt,OU=Virtual Workstations,OU=Win10,OU=Windows,OU=Workstations,OU=Profidata_Computers,OU=Profidata,DC=profidata,DC=com" | Select-Object -ExpandProperty DNSHostName
-#$ComputerNames = Get-ADComputer -Filter * -SearchBase "OU=Luxemburg,OU=Virtual Workstations,OU=Win10,OU=Windows,OU=Workstations,OU=Profidata_Computers,OU=Profidata,DC=profidata,DC=com" | Select-Object -ExpandProperty DNSHostName
-
+#Get All AD Computer Objects
 $ADComputers = Get-ADComputer -Filter * -Properties *
+#Filter AD Computer objetcs for servers
 $ADServers = $ADComputers | Where-Object OperatingSystem -Like "*Server*"
 Write-Debug $ADServers | Format-Table Name, OperatingSystem
-
+#Test if servers are reachable and WINRM is responding
 $TCPTestResults = foreach ($ADServer in $ADServers){
     Test-NetConnection -ComputerName $ADServer.DNSHostName -CommonTCPPort WINRM
 }
-
+#Select reachabel and responding servers
 $OnlineServers = $TCPTestResults | Where-Object TCPTestSucceeded -EQ $true
-
-
 $ScriptBlock = {
     $Value = Test-Path "$($env:ProgramData)\chocolatey\choco.exe"
         if ($Value){
