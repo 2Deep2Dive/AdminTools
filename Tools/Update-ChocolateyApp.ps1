ae678d22d6b49250d4c62758a4d16ec875836025
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2022-04-21 12:39:40 +0200

    new version to update choco core apps

--- Tools/Update-ChocolateyApp.ps1
+++ Tools/Update-ChocolateyApp.ps1
@@ -1,28 +1,42 @@
 function Update-ChocolateyApp{
     param(
         [pscredential] $Cred,
-        [String]$AppName,
-        [String[]] $ComputerNames
+        [String]$AppName
     )
-    foreach($ComputerName in $ComputerNames){
+    #Get All AD Computer Objects
+    #$ADComputers = Get-ADComputer -Filter * -Properties *
+    $ADComputers = Get-ADComputer -Filter * -Properties * -SearchBase 'OU=Virtual Workstations,OU=Win10,OU=Windows,OU=Workstations,OU=Profidata_Computers,OU=Profidata,DC=profidata,DC=com' | Select-Object DNSHostName,OperatingSystem
+    #Filter AD Computer objetcs for servers
+    $FilteredComputers = $ADComputers | Where-Object OperatingSystem -Like "*Windows 10 Pro*"
+    Write-Information $FilteredComputers | Format-Table Name, OperatingSystem
+    $DSNHostNames = $FilteredComputers | Select-Object -ExpandProperty DNSHostName
+    #Test if servers are reachable and WINRM is responding
+    $TCPTestResults = foreach ($DSNHostName in $DSNHostNames){
+        Write-Output "Connecting to $DSNHostName"
+        Test-NetConnection -ComputerName $DSNHostName -CommonTCPPort WINRM -ErrorAction SilentlyContinue -InformationLevel Quiet
+    }
+    $OnlineComputers = $TCPTestResults | Where-Object TCPTestSucceeded -EQ $true
+    foreach($Computer in $OnlineComputers){
         try {
+            Write-Debug "Starting remote job on $Computer"
             Invoke-Command -ComputerName $ComputerName -Credential $Cred -ScriptBlock {
                 #If Chocolaty is not installed then install it
-                if((Test-Connection -Ping -TargetName $ComputerName -Count 1)){
-                    HOSTNAME;
+                HOSTNAME;
+                Write-Information "Checking Chocolatey"
+                $ChocoVersion = choco
+                if($ChocoVersion[0] -like "*Business*"){
                     Write-host "Ugrading $AppName" -ForegroundColor red
-                    choco upgrade $AppName -y --skip-if-not-installed
+                    choco upgrade $AppName -y --no-progress
                 }
-                else {
-                    HOSTNAME;
-                    Write-host "$ComputerName is not reachable" -ForegroundColor Green
-                }
-            }
+            } -AsJob
         }
         catch {
-            Write-Host "$AppName Upgrade Faild!!"
-
+            Write-Host "$Computer Upgrade Faild!!"
         }
-
     }
 }
+
+$AppName = "'chocolatey-license' 'chocolatey' 'chocolatey-agent' 'chocolatey.extension' 'chocolateygui' 'chocolateygui.extension'"
+$Cred = (New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList  'profidata\it-support', (ConvertTo-SecureString -String 'Profidata$2020' -AsPlainText -Force))
+
+Update-ChocolateyApp -Cred $Cred -AppName $AppName
