6a0028bc7d27f997b085a740ee799acf0e38ce7e
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2022-07-18 19:32:38 +0200

    new script to register interl PS repos

---
+++ Tools/Register-PDPSRepo.ps1
@@ -0,0 +1,28 @@
+$Cred = Get-Credential -Credential "profidata\Administrator"
+$ComputerNames = "PDSCVMM02.profidata.com"
+foreach($ComputerName in $ComputerNames ){
+    if((Test-Connection -ComputerName $ComputerName  -Count 1 -ErrorAction SilentlyContinue)){
+	    Write-Warning "Invoking remote command on $ComputerName"
+        try{
+	        Invoke-Command -ComputerName $ComputerName -Credential $Cred  -Authentication Credssp -ScriptBlock {
+		        $Users = Get-ChildItem "C:\Users\" | Select-Object -ExpandProperty Name
+		        ForEach ($User in $Users)
+		        {
+			        $UserPSRepoXMLPath = "c:\Users\$User\AppData\Local\Microsoft\Windows\PowerShell\PowerShellGet\"
+			        IF ((Test-Path $UserPSRepoXMLPath))
+			        {
+                        Copy-Item -Path "\\profidata.com\SYSVOL\profidata.com\scripts\PSRepositories.xml" -Destination "c:\Users\$User\AppData\Local\Microsoft\Windows\PowerShell\PowerShellGet\" -Force -Verbose
+                        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+			        }
+		        }
+		    }
+        }
+        catch{
+            throw $_
+            Write-Error "Faild to Invoke Command on $ComputerName!!"
+        }
+    }
+    else{
+        Write-Error "Failed to reach $ComputerName"
+    }
+}
