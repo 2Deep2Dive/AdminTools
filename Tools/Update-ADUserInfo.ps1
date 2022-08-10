410e6fb9dbaa200b945aea51b5c0da22ef7bbc82
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2022-08-10 06:23:40 +0200

    changed UTF format, add update title

--- Tools/Update-ADUserInfo.ps1
+++ Tools/Update-ADUserInfo.ps1
@@ -1,6 +1,6 @@
-$UsersList = Import-Csv C:\Users\administrator\Documents\export_ad_users_info_updated.csv -Delimiter ';' -Encoding UTF32
+$UsersList = Import-Csv C:\Users\administrator\Documents\export_ad_users_info_updated.csv -Delimiter ';' -Encoding UTF8
 ForEach($UserInfo in $UsersList){
     $ADUser = Get-ADUser -Filter "Name -eq '$($UserInfo.Name)'"
     $ADUserManager = Get-ADUser -Filter "Name -eq '$($UserInfo.Manager)'"
-    $ADUser| Set-ADUser -Department $UserInfo.Department -Company $UserInfo.Company -City $UserInfo.City -Manager $ADUserManager -Verbose
+    $ADUser| Set-ADUser -Department $UserInfo.Department -Title $UserInfo.Title -Company $UserInfo.Company -City $UserInfo.City -Manager $ADUserManager -Verbose
 }
