0f3df2c3a561aa23c35b22be49090d7e4d0e23f5
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2023-12-05 17:02:42 +0100

    removed to Azure repo

--- Tools/Azure/New-DevToolsVM.ps1
+++
@@ -1,1 +0,0 @@
-New-AzResourceGroupDeployment -Name "DevTools_TestVMDeployment" -ResourceGroupName 'PDDEVTools' -TemplateFile .\Tools\Azure\DevTools_Test_VM\template.json -TemplateParameterFile .\Tools\Azure\DevTools_Test_VM\parameters.json -Verbose -Force
